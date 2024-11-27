d4316d40ed6dd20f0a8fe0fee200e51f4b6532dd
NazarSenchuk <113719257+NazarSenchuk@users.noreply.github.com>
2024-11-27 18:04:21 +0000

    Delete aws_instances.tf

--- aws_instances.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "jenkins" {
-    ami = "ami-0866a3c8686eaeeba"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.subnet_public_1.id
-    security_groups= [ aws_security_group.allow_http_ssh.id]
-    key_name ="linux"
-}
